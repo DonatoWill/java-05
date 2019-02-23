c6eece9e6a09a0aa4e9921eb848831d75d9d4fdd
Donato William Gomes Aguiar <donato_will@hotmail.com>
2019-02-22 22:31:58 -0300

    Update Scripts.java

--- src/main/java/challenge/Scripts.java
+++ src/main/java/challenge/Scripts.java
@@ -22,30 +22,11 @@ public final class Scripts implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-   /* @Column(name = "episode")
-    private Integer episode;
-    @Column(name = "episode_name")
-    private String episodeName;
-    @Column(name = "segment")
-    private String segment;
-    @Column(name = "type")
-    private String type;*/
     @Column(name = "actor")
     private String actor;
-   /* @Column(name = "character")
-    private String character;*/
     @Column(name = "detail")
     private String detail;
-   /* @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @Column(name = "record_date")
-    private Timestamp recordDate;
-    @Column(name = "series")
-    private String series;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @Column(name = "transmission_date")
-    private Timestamp transmissionDate;*/
-
-
+  
     public static long getSerialVersionUID() {
         return serialVersionUID;
     }
@@ -53,42 +34,6 @@ public final class Scripts implements Serializable {
     public Integer getId() {
         return id;
     }
-
-   /* public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getEpisode() {
-        return episode;
-    }
-
-    public void setEpisode(Integer episode) {
-        this.episode = episode;
-    }*/
-
-   /* public String getEpisodeName() {
-        return episodeName;
-    }
-
-    public void setEpisodeName(String episodeName) {
-        this.episodeName = episodeName;
-    }
-
-    public String getSegment() {
-        return segment;
-    }
-
-    public void setSegment(String segment) {
-        this.segment = segment;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }*/
 
     public String getActor() {
         return actor;
@@ -98,14 +43,6 @@ public final class Scripts implements Serializable {
         this.actor = actor;
     }
 
-   /* public String getCharacter() {
-        return character;
-    }
-
-    public void setCharacter(String character) {
-        this.character = character;
-    }
-*/
     public String getDetail() {
         return detail;
     }
@@ -113,30 +50,5 @@ public final class Scripts implements Serializable {
     public void setDetail(String detail) {
         this.detail = detail;
     }
-
-    /*public Timestamp getRecordDate() {
-        return recordDate;
-    }
-
-    public void setRecordDate(Timestamp recordDate) {
-        this.recordDate = recordDate;
-    }
-
-    public String getSeries() {
-        return series;
-    }
-
-    public void setSeries(String series) {
-        this.series = series;
-    }
-
-    public Timestamp getTransmissionDate() {
-        return transmissionDate;
-    }
-
-    public void setTransmissionDate(Timestamp transmissionDate) {
-        this.transmissionDate = transmissionDate;
-    }
-*/
 
 }
